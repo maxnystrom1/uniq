5c2109a5f213ddc73b14940a7c3d3da8e1befbce
maxnystrom1 <maxnystrom1@icloud.com>
2025-10-23 15:09:07 +0200

    fix: add ui package entrypoint

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,5 @@
+export { default as Container } from './components/Container.astro';
+export { default as Button } from './components/Button.astro';
+export { default as Card } from './components/Card.astro';
+export { default as Hero } from './sections/Hero.astro';
+export { default as Features } from './sections/Features.astro';
